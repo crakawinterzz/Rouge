12f68518a09982e3c2e9e6a41039a8eafeab8a19
DESKTOP-9SJL8IS\Winterzz <mpwinters86@gmail.com>
2017-01-28 13:27:40 -0800

    Switch Computers

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,23 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
-{
+public class GameManager : MonoBehaviour {
 
+  public static GameManager instance = null;
   public BoardManager boardScript;
+  public int playerFoodPoints = 100;
+  [HideInInspector] public bool playersTurn = true;
 
   private int level = 3;
 
 	// Use this for initialization
-	void Awake ()
-	{
+	void Awake () {
+	  if (instance == null) {
+	    instance = this;
+	  } else if (instance != null) {
+	    Destroy(gameObject);
+	  }
+
+    DontDestroyOnLoad(gameObject);
 	  boardScript = GetComponent<BoardManager>();
 	  InitGame();
 	}
 
-  void InitGame()
-  {
+  void InitGame() {
     boardScript.SetupScene(level);
+  }
+
+  public void GameOver() {
+    enabled = false;
   }
 	
 	// Update is called once per frame
